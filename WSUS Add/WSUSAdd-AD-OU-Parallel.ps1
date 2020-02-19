60c73d89283106138ef9cabd41e1c0a35a4fc42d
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-19 18:20:18 -0500

    calrification

--- WSUS Add/WSUSAdd-AD-OU-Parallel.ps1
+++ WSUS Add/WSUSAdd-AD-OU-Parallel.ps1
@@ -2,6 +2,6 @@ $citrix = Read-Host "Is it a Citrix Group? (Y/N)"
 $group = Read-Host "Enter Security Group Name"
 
 If ($citrix -eq "Y") {
-    get-adgroupmember -Identity "Citrix OU DN HERE" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel.ps1'}
+    get-adgroupmember -Identity "$citrix OU DN HERE" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel.ps1'}
 Else {
-    get-adgroupmember -Identity "OU DN HERE" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel.ps1'}
+    get-adgroupmember -Identity "$group DN HERE" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel.ps1'}
