4ba190b15c701912f2a3a046a643028236d3a048
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-19 18:19:59 -0500

    Clarification

--- WSUS Add/WSUSAdd-AD-OU-Parallel-Turnon.ps1
+++ WSUS Add/WSUSAdd-AD-OU-Parallel-Turnon.ps1
@@ -2,6 +2,6 @@ $citrix = Read-Host "Is it a Citrix Group? (Y/N)"
 $group = Read-Host "Enter Security Group Name"
 
 If ($citrix -eq "Y") {
-    get-adgroupmember -Identity "Citrix OU DN Here" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel-Turnon.ps1'}
+    get-adgroupmember -Identity "$citrix OU DN Here" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel-Turnon.ps1'}
 Else {
-    get-adgroupmember -Identity "OU DN HERE" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel-Turnon.ps1'}
+    get-adgroupmember -Identity "$group DN HERE" | Select -Exp Name | & '.\WSUSAdd-Process-Parallel-Turnon.ps1'}
